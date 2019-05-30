30ce90546b4a3a19c1afaac17be115e55846086e
Shukq <alejim94@gmail.com>
2019-05-29 22:15:13 -0600

    Sign out from mainActivity, navigation from tabs done

--- app/src/main/java/com/jtoru/project2/Actitivies/MainActivity.kt
+++ app/src/main/java/com/jtoru/project2/Actitivies/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.jtoru.project2.Actitivies
 
-import android.content.Context
+
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.design.widget.TabLayout
-import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.view.Menu
+import com.firebase.ui.auth.AuthUI
 import com.jtoru.project2.Fragments.*
 import com.jtoru.project2.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewPager: ViewPager
     private var TAG:String = "MainActivity"
     private lateinit var mFragmentMainAdapter: FragmentMainAdapter
+    private lateinit var tabLay:TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,14 +25,14 @@ class MainActivity : AppCompatActivity() {
         mFragmentMainAdapter = FragmentMainAdapter(supportFragmentManager)
         viewPager = findViewById(R.id.viewpager)
         setupViewPager(viewPager)
-        var tabLayout:TabLayout = findViewById(R.id.tabs)
-        tabLayout.setupWithViewPager(viewPager)
-        tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_art_track_black_24dp)
-        tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_people_black_24dp)
-        tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_notifications_none_black_24dp)
-        tabLayout.getTabAt(3)?.setIcon(R.drawable.ic_menu_black_24dp)
-        tabLayout.getTabAt(0)?.icon?.setTint(resources.getColor(R.color.colorPrimary, null))
-        tabLayout.addOnTabSelectedListener(object : TabLayout.BaseOnTabSelectedListener<TabLayout.Tab>{
+        tabLay = findViewById(R.id.tabs)
+        tabLay.setupWithViewPager(viewPager)
+        tabLay.getTabAt(0)?.setIcon(R.drawable.ic_art_track_black_24dp)
+        tabLay.getTabAt(1)?.setIcon(R.drawable.ic_people_black_24dp)
+        tabLay.getTabAt(2)?.setIcon(R.drawable.ic_notifications_none_black_24dp)
+        tabLay.getTabAt(3)?.setIcon(R.drawable.ic_menu_black_24dp)
+        tabLay.getTabAt(0)?.icon?.setTint(resources.getColor(R.color.colorPrimary, null))
+        tabLay.addOnTabSelectedListener(object : TabLayout.BaseOnTabSelectedListener<TabLayout.Tab>{
             override fun onTabReselected(p0: TabLayout.Tab?) {
             }
 
@@ -59,5 +61,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.search_menu, menu)
         return true
+    }
+
+    override fun onBackPressed() {
+        if(tabLay.selectedTabPosition == 0)
+        {
+            val dialogBack = AlertDialog.Builder(this)
+            dialogBack.setTitle("Exit")
+            dialogBack.setMessage("Do you want to close session?")
+            dialogBack.setPositiveButton("Yes"){
+                    dialog,_ ->
+                dialog.dismiss()
+                finish()
+                AuthUI.getInstance().signOut(this)
+                finishAffinity()
+                val i = Intent(this, HomeActivity::class.java)
+                startActivity(i)
+            }
+            dialogBack.setNegativeButton("No"){
+                    dialog,_ ->
+                dialog.dismiss()
+            }
+            val d = dialogBack.create()
+            d.show()
+        }
+        else
+        {
+            viewPager.currentItem = 0
+        }
     }
 }
